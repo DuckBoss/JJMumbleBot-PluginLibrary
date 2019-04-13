2d87045ca3278a953021f4de1b4522a21beed065
Jason (제이슨) <jasonjerome16@gmail.com>
2019-04-13 01:08:46 -0400

    Updated plugin_template for v1.7.0+

--- plugin_template.py
+++ plugin_template.py
@@ -18,4 +18,26 @@ class PluginBase(ABC):
 
     @abstractmethod
     def help(self):
+        return help_data
+
+    @abstractmethod 
+    def get_plugin_version(self):
+        return plugin_version
+
+    @abstractmethod
+    def get_priv_path(self):
+        return priv_path
+
+    @property
+    @abstractmethod
+    def help_data(self):
+        pass
+
+    @property
+    @abstractmethod
+    def plugin_version(self):
+        pass
+
+    @property
+    def priv_path(self):
         pass
