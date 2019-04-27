211845ed9bf872d034531e2df2ffdeb2c82fe558
Jason (제이슨) <jasonjerome16@gmail.com>
2019-04-27 14:45:33 -0400

    Updated for v2.0.0

--- plugins/lmgtfy/lmgtfy.py
+++ plugins/lmgtfy/lmgtfy.py
@@ -1,33 +1,31 @@
 from templates.plugin_template import PluginBase
-from helpers.global_access import debug_print, reg_print
-import utils
+from helpers.global_access import debug_print
+from helpers.global_access import GlobalMods as GM
 import requests
 import privileges as pv
 
 
 class Plugin(PluginBase):
-    help_data = "<br><b><font color='red'>#####</font> LMGTFY Plugin Help <font color='red'>#####</font></b><br> \
-                    All commands can be run by typing it in the channel or privately messaging DuckBot.<br>\
+    help_data = "All commands can be run by typing it in the channel or privately messaging DuckBot.<br>\
                     <b>!lmgtfy 'message'</b>: Let me google that for you..."
-    plugin_version = "1.6.0"
+    plugin_version = "2.0.0"
     priv_path = "lmgtfy/lmgtfy_privileges.csv"
 
     def __init__(self):
         debug_print("LMGTFY Plugin Initialized.")
         super().__init__()
 
-    def process_command(self, mumble, text):
+    def process_command(self, text):
         message = text.message.strip()
         message_parse = message[1:].split(' ', 1)
         command = message_parse[0]
 
         if command == "lmgtfy":
-            if not pv.plugin_privilege_checker(mumble, text, command, self.priv_path):
+            if not pv.plugin_privilege_checker(text, command, self.priv_path):
                 return
             parameter = message_parse[1]
             results = self.lmgtfy(parameter)
-            utils.echo(mumble.channels[mumble.users.myself['channel_id']],
-                                     "<a href='%s'>%s</a>" % (results, results))
+            GM.gui.quick_gui(f"<a href='{results}'>{results}</a>", text_type='header', box_align='left')
 
     def lmgtfy(self, query):
         lmgtfy_url = 'http://lmgtfy.com/?q=' + '+'.join([word.replace(" ", "+") for word in query.split()])
@@ -35,9 +33,11 @@ class Plugin(PluginBase):
         r = requests.get('http://is.gd/create.php', params=payload)
         return r.json()['shorturl']
 
-    @staticmethod
-    def plugin_test():
+    def plugin_test(self):
         debug_print("LMGTFY Plugin self-test callback.")
+
+    def is_audio_plugin(self):
+        return False
 
     def quit(self):
         debug_print("Exiting LMGTFY Plugin...")
